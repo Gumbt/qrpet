b608e8e3e6cceb0e8cf1d1f957d18df90073e561
Gumb <gustavo.bonassa1@gmail.com>
2019-06-09 01:11:45 -0300

    contato adc

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '',
-    database: 'qrpet',
+    host: 'sql176.main-hosting.eu',
+    username: 'u861781140_coc',
+    password: '1234567g',
+    database: 'u861781140_coc',
     operatorAliase: false,
     define: {
         timestamps: true, //data de criação
